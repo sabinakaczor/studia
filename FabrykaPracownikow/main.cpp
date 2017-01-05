666d1bc206bc7528b234ca0078848fe09648223a
sabinakaczor <sabina.gbw@gmail.com>
2017-01-06 00:50:00 +0100

    Aktualizacja 6.01

--- FabrykaPracownikow/main.cpp
+++ FabrykaPracownikow/main.cpp
@@ -3,145 +3,172 @@
 
 using namespace std;
 
-//klasy z pracownikami
 class Person
 {
 public:
-    virtual vector<int> get_rooms() = 0;
+    vector<int> get_rooms()
+    {
+        return this->rooms;
+    }
+
+    void set_rooms(vector<int> rooms)
+    {
+        this->rooms = rooms;
+    }
+
+private:
+    vector<int> rooms;
 };
 
 class Cleaner : public Person
 {
 public:
-    vector<int> get_rooms() {
-        return this->rooms;
-    }
+
 };
 
 class Receptionist : public Person
 {
 public:
-    vector<int> get_rooms() {
-        return this->rooms;
-    }
+
 };
 
 class Boss : public Person
 {
 public:
-    vector<int> get_rooms() {
-        return this->rooms;
-    }
+
 };
 
 class Admin : public Person
 {
 public:
-    vector<int> get_rooms() {
-        return this->rooms;
-    }
+
 };
 
 class Guest : public Person
 {
 public:
-    vector<int> get_rooms() {
-        return this->rooms;
-    }
+
 };
 
-//fabryki pracowników
 class EmployeeFactory
 {
 public:
-    virtual Person* createEmployee() = 0;
-protected:
-    virtual vector<int> set_rooms() = 0;
-};
-
-class CleanerFactory : public EmployeeFactory
-{
-public:
-    Person* createEmployee() {
-        return new Cleaner();
+    EmployeeFactory()
+    {
+        last_free_room = 5;
+        basement_keys = 1;
+        rooftop_keys = 40;
+        boss_room_keys = 20;
+        stock_room_keys = 2;
+        conference_room_keys = 11;
+        laundry_keys = 4;
+        cleaner_room_keys = 3;
     }
-private:
-    vector<int> set_rooms() {
-        vector<int> rooms;
-        for(int i=3; i<=39; ++i) {
-            if (i != 20)
-                rooms.push_back(i);
+
+    Person* createEmployee(string name)
+    {
+        Person *ret;
+        vector<int> keys;
+        if (name == "szef") {
+            ret = new Boss();
+            keys.push_back(boss_room_keys);
+            keys.push_back(stock_room_keys);
+            keys.push_back(rooftop_keys);
+            keys.push_back(basement_keys);
+            ret->set_rooms(keys);
+        } else if (name == "admin") {
+            // stworzyc administratora
+            // nadac klucze
+            ret = new Admin();
+            for (int i=1; i<=40; ++i) {
+                if (i!=boss_room_keys)
+                    keys.push_back(i);
+            }
+            ret->set_rooms(keys);
+        } else if (name == "sprzataczka") {
+            // stworzyc sprzataczke
+            // nadac klucze
+            ret = new Cleaner();
+            keys.push_back(cleaner_room_keys);
+            keys.push_back(laundry_keys);
+            keys.push_back(conference_room_keys);
+            for (int i=5; i<=39; ++i) {
+                if (i!=conference_room_keys && i!=boss_room_keys)
+                    keys.push_back(i);
+            }
+            ret->set_rooms(keys);
+        } else if (name == "recepcjonistka") {
+            // stworzyc recepcjonistke
+            // nadac klucze
+            ret = new Receptionist();
+            keys.push_back(stock_room_keys);
+            keys.push_back(conference_room_keys);
+            for (int i=5; i<=39; ++i) {
+                if (i!=conference_room_keys && i!=boss_room_keys)
+                    keys.push_back(i);
+            }
+            ret->set_rooms(keys);
+        } else if (name == "gosc") {
+            // stworzyc goscia
+            // nadac klucze
+            ret = new Guest();
+            keys.push_back(get_next_guest_room_key());
+            ret->set_rooms(keys);
         }
-        return rooms;
-    }
-};
-
-class ReceptionistFactory : public EmployeeFactory
-{
-public:
-    Person* createEmployee() {
-        return new Receptionist();
+        return ret;
     }
 private:
-    vector<int> set_rooms() {
-        vector<int> rooms;
-        for(int i=2; i<=39; ++i) {
-            if (i != 3 && i != 4 && i != 20)
-                rooms.push_back(i);
+    int last_free_room;
+    int basement_keys;
+    int rooftop_keys;
+    int boss_room_keys;
+    int stock_room_keys;
+    int conference_room_keys;
+    int laundry_keys;
+    int cleaner_room_keys;
+
+    int get_next_guest_room_key()
+    {
+        while (last_free_room == boss_room_keys ||
+               last_free_room == rooftop_keys ||
+               last_free_room == conference_room_keys) {
+            last_free_room += 1;
         }
-        return rooms;
+        return last_free_room;
     }
-};
 
-class BossFactory : public EmployeeFactory
-{
-public:
-    Person* createEmployee() {
-        return new Boss();
-    }
-private:
-    vector<int> set_rooms() {
-        vector<int> rooms;
-        rooms.push_back(1);
-        rooms.push_back(2);
-        rooms.push_back(20);
-        rooms.push_back(40);
-        return rooms;
-    }
-};
-
-class AdminFactory : public EmployeeFactory
-{
-public:
-    Person* createEmployee() {
-        return new Admin();
-    }
-private:
-    vector<int> set_rooms() {
-        vector<int> rooms;
-        for(int i=1; i<=40; ++i) {
-            if (i != 20)
-                rooms.push_back(i);
+    vector<int> get_all_guest_room_keys()
+    {
+        vector<int> ret;
+        int i = 5;
+        for (int i=5; i < 40; ++i) {
+            if (!(i == boss_room_keys ||
+                   i == rooftop_keys ||
+                   i == conference_room_keys)) {
+                ret.push_back(i);
+            }
         }
-        return rooms;
-    }
-};
-
-class GuestFactory : public EmployeeFactory
-{
-public:
-    Person* createEmployee() {
-        return new Guest();
-    }
-private:
-    vector<int> set_rooms() {
-        vector<int> rooms;
-        rooms.push_back(this->room_no);
-        return rooms;
+        return ret;
     }
 };
 
 int main()
 {
+    EmployeeFactory *ef = new EmployeeFactory();
+    Person *sprzataczka = ef->createEmployee("sprzataczka");
+    Person *szef = ef->createEmployee("szef");
+    Person *recepcjonistka = ef->createEmployee("recepcjonistka");
+    vector<int> spr = sprzataczka->get_rooms();
+    vector<int> sz = szef->get_rooms();
+    vector<int> rec = recepcjonistka->get_rooms();
+    cout << "SprzÄ…taczka: ";
+    for(int i=0; i<spr.size(); ++i)
+        cout << spr[i] << " ";
+    cout << "\nSzef: ";
+    for(int i=0; i<sz.size(); ++i)
+        cout << sz[i] << " ";
+    cout << "\nRecepcjonistka: ";
+    for(int i=0; i<rec.size(); ++i)
+        cout << rec[i] << " ";
     return 0;
 }
--- generator_dokumentow/main.cpp
+++ generator_dokumentow/main.cpp
@@ -1,13 +1,219 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 //scenariusz: artykul / blog
 // artykul w ascii, blog w html
 //builder: html / tekstowy
 
+/*Wykorzystując wzorzec budowniczy stwórz generator dokumentów tekstowych i HTML.
+Generator powinien obsługiwać:
+dodawanie paragrafów
+<p>tekst paragrafu</p> dla html, tekst paragrafu dla dokumentu tekstowego
+dodawanie linii poziomych
+<hr> dla html, “----------------------------” dla dokumentu tekstowego
+rozpoczynanie dokumentu
+<html> dla html, brak działania dla dokumentu tekstowego
+kończenie dokumentu
+</html> dla html, brak działania dla dokumentu tekstowego
+Napisz program generujacy tekst dla artykulu i dla blogu pobierając listę tekstów z pliku (wczytujemy linia po linii),
+i generujący wynik odpowiednio dla artykułu:
+linia pozioma
+linia tekstu
+linia pozioma
+linia tekstu
+....
+
+i dla blogu:
+linia pozioma
+linia pozioma
+linia pozioma
+linia tekstu
+linia pozioma
+linia pozioma
+linia pozioma
+linia tekstu
+....*/
+
 using namespace std;
 
+class Dokument
+{
+public:
+    Dokument() {}
+    void show_text() {
+        //cout << this->edited_text.size();
+        for(int i=0; i<this->edited_text.size(); ++i)
+            cout << edited_text[i] << endl;
+    }
+    vector<string> get_text() { return this->edited_text; }
+    void dodaj_element(string s) { this->edited_text.push_back(s); }
+private:
+    vector<string> edited_text;
+};
 
-class Document
+class DokumentBuilder
+{
+protected:
+    Dokument *dok;
+public:
+    DokumentBuilder() {}
+    virtual void edit_text(vector<string> text) = 0;
+    void start_editing_text() { this->dok = new Dokument(); }
+    Dokument* get_dokument() { return this->dok; }
+    virtual void rozpocznij_paragraf() = 0;
+    virtual void zakoncz_paragraf() = 0;
+    virtual void dodaj_linie_pozioma() = 0;
+    virtual void rozpocznij_dokument() = 0;
+    virtual void zakoncz_dokument() = 0;
+    virtual void dodaj_linie(string s) = 0;
+};
+
+class DokumentHTMLBuilder : public DokumentBuilder
+{
+public:
+    DokumentHTMLBuilder() : DokumentBuilder() {}
+    void rozpocznij_paragraf() {
+        dok->dodaj_element("<p>");
+    }
+    void zakoncz_paragraf() {
+        dok->dodaj_element("</p>");
+    }
+    void dodaj_linie_pozioma() {
+        dok->dodaj_element("<hr>");
+    }
+    void rozpocznij_dokument() {
+        dok->dodaj_element("<html>");
+    }
+    void zakoncz_dokument() {
+        dok->dodaj_element("</html>");
+    }
+    void dodaj_linie(string s) {
+        dok->dodaj_element(s);
+    }
+    void edit_text(vector<string> text) {
+        cout << text.size();
+        this->rozpocznij_dokument();
+        for (int i=0; i<text.size(); ++i) {
+            this->dodaj_linie_pozioma();
+            this->dodaj_linie_pozioma();
+            this->dodaj_linie_pozioma();
+            if (i==0)
+                this->rozpocznij_paragraf();
+            else {
+                if (text[i].size()>0 && text[i-1].size()==0)
+                    this->rozpocznij_paragraf();
+                else if (text[i].size()==0)
+                    this->zakoncz_paragraf();
+            }
+            this->dodaj_linie(text[i]);
+            if (i==text.size()-1)
+                this->zakoncz_paragraf();
+        }
+        this->zakoncz_dokument();
+    }
+};
+
+class DokumentTXTBuilder : public DokumentBuilder
+{
+public:
+    DokumentTXTBuilder() : DokumentBuilder() {}
+    void rozpocznij_paragraf() {}
+    void zakoncz_paragraf() {}
+    void dodaj_linie_pozioma() {
+        dok->dodaj_element("----------------------------");
+    }
+    void rozpocznij_dokument() {}
+    void zakoncz_dokument() {}
+    void dodaj_linie(string s) {
+        dok->dodaj_element(s);
+    }
+    void edit_text(vector<string> text) {
+         cout << text.size();
+        this->rozpocznij_dokument();
+        for (int i=0; i<text.size(); ++i) {
+            this->dodaj_linie_pozioma();
+            this->dodaj_linie(text[i]);
+        }
+        this->zakoncz_dokument();
+    }
+};
+
+class Editor
+{
+public:
+    Editor(vector<string> text) { this->text = text; }
+    void generate_document() {
+        this->db->start_editing_text();
+        this->db->edit_text(this->text);
+    }
+    void set_dokument_builder(DokumentBuilder *db) { this->db = db; }
+    Dokument* get_dokument() { return this->db->get_dokument(); }
+private:
+    DokumentBuilder *db;
+    vector<string> text;
+};
+
+int main()
+{
+    ifstream f;
+    string line;
+    vector<string> text;
+
+    f.open("pure_text.txt",ios::in);
+
+    if(f.is_open()) {
+        //f >> line;
+        getline( f, line );
+        while(!f.eof()) {
+           text.push_back(line);
+            getline( f, line );
+
+            //f >> line;
+        }
+        f.close();
+    }else {
+        cout << "blad otwarcia pure_text.txt";
+    }
+
+    cout << text.size(); //466
+
+    Editor *editor = new Editor(text);
+    DokumentHTMLBuilder *dokhtmlbuilder = new DokumentHTMLBuilder();
+    DokumentTXTBuilder *doktxtbuilder =  new DokumentTXTBuilder();
+
+    editor->set_dokument_builder(dokhtmlbuilder);
+    editor->generate_document();
+    Dokument *dokhtml = editor->get_dokument();
+   // vector<string> edited =
+    vector<string> texthtml = dokhtml->get_text();
+    //dokhtml->show_text();
+
+    ofstream plikhtml;
+    plikhtml.open("text_html.txt");
+    for(int i=0; i<texthtml.size(); ++i)
+        plikhtml<<texthtml[i]<<endl;
+    plikhtml.close();
+
+
+    cout << "\n";
+    editor->set_dokument_builder(doktxtbuilder);
+    editor->generate_document();
+    Dokument *doktxt = editor->get_dokument();
+    vector<string> texttxt = doktxt->get_text();
+    //doktxt->show_text();
+
+    ofstream pliktxt;
+    pliktxt.open("text_txt.txt");
+    for(int i=0; i<texttxt.size(); ++i)
+        pliktxt<<texttxt[i]<<endl;
+    pliktxt.close();
+
+    return 0;
+}
+
+
+/*class Document
 {
 public:
     void setParagraf(string *par) {
@@ -104,11 +310,7 @@ public:
     }
 private:
     Builder *builder;
-};
+};*/
 
 
-int main()
-{
 
-    return 0;
-}
